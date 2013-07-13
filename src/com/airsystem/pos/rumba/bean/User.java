a60050e43c0bef4770dc33e1012f69b459f47ab9
budioktaviyan <budi.oktaviyan@me.com>
2013-07-13 10:08:23 +0700

    Create POJO for User bean

--- src/com/airsystem/pos/rumba/bean/User.java
+++ src/com/airsystem/pos/rumba/bean/User.java
@@ -1,7 +1,49 @@
 package com.airsystem.pos.rumba.bean;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 /**
  * @author Budi Oktaviyan Suryanto <budi.oktaviyan@icloud.com>
  */
+
+@Entity
+@Table(name = "user")
 public class User {
+
+	@Id
+	@Column(name = "username")
+	private String username;
+
+	@Column(name = "password")
+	private String password;
+
+	@Column(name = "role")
+	private String role;
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
 }
