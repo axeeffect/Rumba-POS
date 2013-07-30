eb4cc958d80af92f88727966cc126ae5666cbcb7
budioktaviyan <budi.oktaviyan@me.com>
2013-07-31 01:20:53 +0700

    Add constant object for save and update mode

---
+++ src/com/airsystem/pos/rumba/util/Constant.java
@@ -0,0 +1,11 @@
+package com.airsystem.pos.rumba.util;
+
+/**
+ * @author Budi Oktaviyan Suryanto <budi.oktaviyan@icloud.com>
+ */
+public class Constant {
+
+	public static final int MODE_CREATE = 0;
+
+	public static final int MODE_UPDATE = 1;
+}
